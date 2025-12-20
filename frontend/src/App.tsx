d7c310c7acc03242a4cb6516968babf17c974b28
ZaferSarioglu <sariogluuzafer@gmail.com>
2025-12-20 13:59:37 +0100

    api.ts in Frontend and some basic routing in app.tsx implemented.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import api from "./services/api"
 
-function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:3000")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
+function TemporaryHomePage({ message }: { message: string }) {
   return (
     <div>
       <h1> Hello World from PawPal </h1>
       <p>Message from Express backend, connected to Vite+React frontend:</p>
       <p>{message}</p>
     </div>
+  );
+}
+
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    api.get("/").then((res) => setMessage(res.data.message));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TemporaryHomePage message={message} />} />
+        {/* <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} /> These are for future use*/}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
